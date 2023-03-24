89f0ef022dd66a5a3cb9e6e85be0de67ae5f144c
haryang Park <haryang92@gmail.com>
2023-03-24 23:18:22 +0900

    Changes by landsoft

--- src/main/java/com/example/Board/domain/post/PostResponse.java
+++ src/main/java/com/example/Board/domain/post/PostResponse.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-@Getter
+//@Getter
 public class PostResponse {
     private Long id;                       // PK
     private String title;                  // 제목
@@ -15,4 +15,33 @@ public class PostResponse {
     private Boolean deleteYn;              // 삭제 여부
     private LocalDateTime createdDate;     // 생성일시
     private LocalDateTime modifiedDate;    // 최종 수정일시
+
+    public Long getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getContent() {
+        return content;
+    }
+    public String getWriter() {
+        return writer;
+    }
+    public int getViewCnt() {
+        return viewCnt;
+    }
+    public Boolean getNoticeYn() {
+        return noticeYn;
+    }
+    public Boolean getDeleteYn() {
+        return deleteYn;
+    }
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+
 }
